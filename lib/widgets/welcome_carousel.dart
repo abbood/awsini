2ee0cdc2d3802bcee54fce128b2c4c913f5a5ad9
abdulah bakhach <abdullah.bakhach@gmail.com>
2024-10-01 10:49:46 +0300

    make coursel work

--- lib/widgets/welcome_carousel.dart
+++ lib/widgets/welcome_carousel.dart
@@ -17,109 +17,140 @@ class _WelcomeCarouselState extends State<WelcomeCarousel> {
 
   final List<Map<String, String>> _carouselItems = [
     {
-      "title": "Welcome",
-      "description": "Discover amazing wallpapers for your device."
+      "description": "welcome to Awsini, your custom Islamic wallpaper app",
+      "image": "assets/wallpaper8_detail.png"
     },
     {
-      "title": "Browse",
-      "description": "Explore our vast collection of high-quality wallpapers."
+      "description":
+          "Although the web is full of amazing Arabic calligraphy, the resolution isn't custom made for your phone",
+      "image": "assets/low-rez-calligraphy.png"
     },
     {
-      "title": "Customize",
-      "description": "Find the perfect wallpaper to match your style."
+      "description":
+          "At Awsini, we work with the best calligraphers in the Muslim world to build content that is specifically suitable for phone wallpapers",
+      "image": "assets/calligrapher-desktop.png"
     },
     {
-      "title": "Get Started",
-      "description": "Ready to begin your wallpaper journey?"
+      "description":
+          "And then at our digital labs, we convert the hand drawn calligraphy into digital formats that scale beautifully for every phone resolution!",
+      "image": "assets/calligraphy-conversion.png"
+    },
+    {
+      "description":
+          "The end result is a beautiful calligraphy that is both a reminder for you and an adornment for your phone",
+      "image": "assets/wallpaper9_detail.png"
     },
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          CarouselSlider.builder(
-            itemCount: _carouselItems.length,
-            carouselController: _carouselController,
-            options: CarouselOptions(
-              height: MediaQuery.of(context).size.height,
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              enableInfiniteScroll: false,
-              onPageChanged: (index, reason) {
-                setState(() {
-                  _currentIndex = index;
-                });
-              },
-            ),
-            itemBuilder: (BuildContext context, int index, int realIndex) {
-              final item = _carouselItems[index];
-              return Container(
-                width: MediaQuery.of(context).size.width,
-                color: Colors.blue,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      item['title']!,
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                    SizedBox(height: 20),
-                    Padding(
-                      padding: EdgeInsets.symmetric(horizontal: 20),
-                      child: Text(
-                        item['description']!,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(fontSize: 18, color: Colors.white),
-                      ),
-                    ),
-                    if (index == _carouselItems.length - 1)
-                      Padding(
-                        padding: EdgeInsets.only(top: 40),
-                        child: ElevatedButton(
-                          child: Text('Start'),
-                          onPressed: widget.onComplete,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.white,
-                            foregroundColor: Colors.blue,
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 50, vertical: 15),
-                          ),
-                        ),
-                      ),
-                  ],
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: CarouselSlider.builder(
+                itemCount: _carouselItems.length,
+                carouselController: _carouselController,
+                options: CarouselOptions(
+                  height: MediaQuery.of(context).size.height,
+                  viewportFraction: 1.0,
+                  enlargeCenterPage: false,
+                  enableInfiniteScroll: false,
+                  onPageChanged: (index, reason) {
+                    setState(() {
+                      _currentIndex = index;
+                    });
+                  },
                 ),
-              );
-            },
-          ),
-          Positioned(
-            bottom: 20,
-            left: 0,
-            right: 0,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: _carouselItems.asMap().entries.map((entry) {
-                return GestureDetector(
-                  onTap: () => _carouselController.animateToPage(entry.key),
-                  child: Container(
-                    width: 8.0,
-                    height: 8.0,
-                    margin: EdgeInsets.symmetric(horizontal: 4.0),
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: Colors.white
-                          .withOpacity(_currentIndex == entry.key ? 0.9 : 0.4),
+                itemBuilder: (BuildContext context, int index, int realIndex) {
+                  final item = _carouselItems[index];
+                  return Padding(
+                    padding: EdgeInsets.fromLTRB(20, 10, 20, 40),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.black,
+                        border: Border.all(color: Colors.white, width: 4),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Expanded(
+                            flex: 2,
+                            child: Padding(
+                              padding: EdgeInsets.all(20),
+                              child: Image.asset(
+                                item['image']!,
+                                fit: BoxFit.contain,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 1,
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Padding(
+                                  padding: EdgeInsets.symmetric(horizontal: 20),
+                                  child: Text(
+                                    item['description']!,
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                        fontSize: 18, color: Colors.white),
+                                  ),
+                                ),
+                                if (index == _carouselItems.length - 1)
+                                  Padding(
+                                    padding: EdgeInsets.only(top: 20),
+                                    child: ElevatedButton(
+                                      child: Text('Start exploring now'),
+                                      onPressed: widget.onComplete,
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white,
+                                        foregroundColor: Colors.black,
+                                        padding: EdgeInsets.symmetric(
+                                            horizontal: 50, vertical: 15),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(5),
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
                     ),
-                  ),
-                );
-              }).toList(),
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: EdgeInsets.only(bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: _carouselItems.asMap().entries.map((entry) {
+                  return GestureDetector(
+                    onTap: () => _carouselController.animateToPage(entry.key),
+                    child: Container(
+                      width: 8.0,
+                      height: 8.0,
+                      margin: EdgeInsets.symmetric(horizontal: 4.0),
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white.withOpacity(
+                            _currentIndex == entry.key ? 0.9 : 0.4),
+                      ),
+                    ),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
