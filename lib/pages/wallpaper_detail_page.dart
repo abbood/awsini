0c4f513c581463f27d8701d73eead0f563b739cf
abdulah bakhach <abdullah.bakhach@gmail.com>
2024-10-03 13:42:49 +0300

    add more social media links

--- lib/pages/wallpaper_detail_page.dart
+++ lib/pages/wallpaper_detail_page.dart
@@ -283,6 +283,9 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
         } else {
           final artistData = snapshot.data!;
           final isDarkMode = Theme.of(context).brightness == Brightness.dark;
+          final hasSignature = artistData['signature_url'] != null &&
+              artistData['signature_url'].toString().trim().isNotEmpty;
+
           return Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
@@ -293,11 +296,14 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
                     radius: 40,
                     backgroundImage: NetworkImage(artistData['picture_url']),
                   ),
-                  SizedBox(width: 16),
-                  CircleAvatar(
-                    radius: 40,
-                    backgroundImage: NetworkImage(artistData['signature_url']),
-                  ),
+                  if (hasSignature) ...[
+                    SizedBox(width: 16),
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage:
+                          NetworkImage(artistData['signature_url']),
+                    ),
+                  ],
                 ],
               ),
               SizedBox(height: 16),
@@ -332,11 +338,9 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
     if (socialMedia == null || socialMedia.isEmpty) {
       return SizedBox.shrink();
     }
-
     final isDarkMode = Theme.of(context).brightness == Brightness.dark;
     final iconColor = isDarkMode ? Colors.grey[400] : Colors.grey[700];
     final textColor = isDarkMode ? Colors.grey[300] : Colors.grey[800];
-
     return Wrap(
       alignment: WrapAlignment.center,
       spacing: 16,
@@ -344,21 +348,47 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
       children: socialMedia.entries.map((entry) {
         IconData icon;
         String url = entry.value;
-        switch (entry.key) {
+        String prefix;
+        String displayText;
+        switch (entry.key.toLowerCase()) {
           case 'facebook':
             icon = FontAwesomeIcons.facebook;
-            url = _processSocialMediaUrl(url, 'https://www.facebook.com');
+            prefix = 'https://www.facebook.com/';
             break;
           case 'youtube':
             icon = FontAwesomeIcons.youtube;
-            url = _processSocialMediaUrl(url, 'https://www.youtube.com');
+            prefix = 'https://www.youtube.com/';
             break;
           case 'instagram':
             icon = FontAwesomeIcons.instagram;
+            prefix = 'https://www.instagram.com/';
+            break;
+          case 'linkedin':
+            icon = FontAwesomeIcons.linkedin;
+            prefix = 'https://www.linkedin.com/in/';
+            break;
+          case 'tiktok':
+            icon = FontAwesomeIcons.tiktok;
+            prefix = 'https://www.tiktok.com/@';
+            break;
+          case 'x':
+          case 'twitter':
+            icon = FontAwesomeIcons.xTwitter;
+            prefix = 'https://twitter.com/';
             break;
           default:
             icon = FontAwesomeIcons.link;
+            prefix = 'https://';
         }
+
+        if (url.toLowerCase().startsWith(prefix.toLowerCase())) {
+          displayText = 'sample';
+        } else {
+          displayText =
+              url.replaceAll(RegExp(r'[/\\]'), ''); // Remove any slashes
+          url = _processSocialMediaUrl(url, prefix);
+        }
+
         return InkWell(
           onTap: () => _launchUrl(url),
           child: Row(
@@ -367,7 +397,7 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
               Icon(icon, size: 16, color: iconColor),
               SizedBox(width: 4),
               Text(
-                entry.key,
+                displayText,
                 style: TextStyle(color: textColor, fontSize: 12),
               ),
             ],
@@ -608,34 +638,12 @@ class _WallpaperDetailPageState extends State<WallpaperDetailPage> {
   }
 
   String _processSocialMediaUrl(String url, String prefix) {
-    if (url.isEmpty) {
-      return prefix;
+    if (url.toLowerCase().startsWith('http://') ||
+        url.toLowerCase().startsWith('https://')) {
+      return url;
+    } else {
+      return '$prefix$url';
     }
-
-    // Remove any leading or trailing whitespace
-    url = url.trim();
-
-    // If the URL starts with 'http://', replace it with 'https://'
-    if (url.startsWith('http://')) {
-      url = 'https://' + url.substring(7);
-    }
-
-    // If the URL doesn't start with 'https://', add it
-    if (!url.startsWith('https://')) {
-      url = 'https://' + url;
-    }
-
-    // If the URL doesn't start with the expected prefix (after adding https://),
-    // assume it's just a username or page name and add it to the prefix
-    if (!url.startsWith(prefix)) {
-      // Remove 'https://www.' or 'https://' if present
-      url = url.replaceAll(RegExp(r'^https?://(www\.)?'), '');
-
-      // Combine the prefix with the processed URL
-      url = prefix + '/' + url;
-    }
-
-    return url;
   }
 }
 
